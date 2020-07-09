1911e541213349545118da9beedd4be6712c9625
Chathuranga Pathirana <cbjpathirana@gmail.com>
2020-07-09 15:18:32 +0800

    first commit

---
+++ LifeBackup.Core/Interfaces/IBucketRepository.cs
@@ -0,0 +1,16 @@
+﻿using LifeBackup.Core.Communication.Bucket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeBackup.Core.Interfaces
+{
+    public interface IBucketRepository
+    {
+        Task<bool> DoesS3BucketExist(string bucketName);
+        Task<CreateBucketResponse> CreateBucket(string bucketName);
+        Task<IEnumerable<ListS3BucketsResponse>> ListBuckets();
+        Task DeleteBucket(string bucketName);
+    }
+}
